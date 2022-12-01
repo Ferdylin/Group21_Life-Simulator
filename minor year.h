a9fe4303345ef64daa370f24284f5047d8c6ce25
Justin8900 <119088721+Justin8900@users.noreply.github.com>
2022-12-01 22:17:48 +0800

    Create minor year.h

---
+++ minor year.h
@@ -0,0 +1,13 @@
+// Header file for the functions related to the minor years
+
+#include <string>
+
+using namespace std;
+
+void resultsEvents14_to_17( int index, Stats &s, ofstream & fout );
+
+void resultsEvents20_to_23( int index, Stats &s, ofstream & fout );
+
+void resultsEvents26_to_29( int index, Stats &s, ofstream & fout );
+
+void resultsEvents32_20_35( int index, Stats &s, ofstream & fout );
