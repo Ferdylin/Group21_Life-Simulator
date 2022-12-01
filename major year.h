1609ba389e56270925766859b2418cd518f4c3ea
Justin8900 <119088721+Justin8900@users.noreply.github.com>
2022-12-01 22:14:29 +0800

    Create major year.h

---
+++ major year.h
@@ -0,0 +1,23 @@
+/*
+FILE: header file for major year 13 18 19 24 25 30 31 36
+*/
+
+#include <string>
+
+using namespace std;
+
+void Events13 ( Stats &s, string name, ofstream & fout );
+
+void Events18 ( Stats &s, string name, ofstream & fout );
+
+void Events19 ( Stats &s, string name, ofstream & fout );
+
+void Events24 ( Stats &s, string name, ofstream & fout );
+
+void Events25 ( Stats &s, string name, ofstream & fout );
+
+void Events30 ( Stats &s, string name, ofstream & fout );
+
+void Events31 ( Stats &s, string name, ofstream & fout );
+
+void Events36 ( Stats &s, string name, ofstream & fout );
